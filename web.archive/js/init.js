1fac9be12112b50fb3014aab788702f0f745bdb3
kei0425 <kei0425@gmail.com>
2013-12-10 10:45:46 +0900

    commit test-cross3.nikkei-r.local

--- web.archive/js/init.js
+++ web.archive/js/init.js
@@ -4,17 +4,37 @@ chrome.webRequest.onHeadersReceived.addListener(
         matches = details.statusLine.match(/^(\S+) (\d\d\d)/) || [],
         statusCode = parseInt(matches[2], 10)
         ;
-        if (statusCode < 200 && 300 <= statusCode) {
-            console.log(details.statusLine + " url:" + details.url);
+        if (statusCode < 200 || 300 <= statusCode) {
+            console.log(details.statusLine + " url:" + details.url +
+                        ' type:' + details.type);
             if (statusCode == 404) {
-                
+                checkWebArchive(details.url, details.tabId);
             }
         }
 
         return;
     },
     {
-        urls: ['*://*/*']
+        urls: ['*://*/*'],
+        types: ['main_frame']
     },
     ["responseHeaders"]
 );
+
+function checkWebArchive(url, tabId) {
+    $.getJSON(
+        'http://archive.org/wayback/available',
+        {
+            url : url
+        },
+        function (json) {
+            if (json.archived_snapshots.closest) {
+                chrome.tabs.update(
+                    tabId,
+                    {
+                        url : json.archived_snapshots.closest.url
+                    });
+            }
+        }
+    );
+}
